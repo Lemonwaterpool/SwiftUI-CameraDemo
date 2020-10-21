b14da6fb29293b67cf74efd2cfd43da139d64ec1
Lemonwaterpool <frqk3624@gmail.com>
2020-10-21 22:52:18 +0900

    Initial Commit

---
+++ Shared/SwiftUI_CameraDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_CameraDemoApp.swift
+//  Shared
+//
+//  Created by H Chan on 2020/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_CameraDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
